c38c297afb2853c3357ea01a07cd985adc69e9e0
gunnchOS3k <127266036+gunnchOS3k@users.noreply.github.com>
2023-04-03 01:44:17 +0000

    new file:   hwcatchup2 	new file:   hwcatchup2.cpp

---
+++ hwcatchup2.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Edmund Gunn Jr
+Device: G-Phone google pixel via codespaces
+Date: 4/2/2023
+Module: 3
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numOne, numTwo, sum;
+
+    cout<<"Please enter two numbers separated by a space"<<endl;
+    cin>>numOne>>numTwo;
+    sum = numOne + numTwo;
+    cout<<numOne<<" + "<<numTwo<<" = "<<sum<<endl;
+    return 0;
+}
