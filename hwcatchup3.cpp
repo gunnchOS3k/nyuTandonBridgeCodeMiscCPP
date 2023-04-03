989bffed17299175f7b667fc5a062af6b8d55940
gunnchOS3k <127266036+gunnchOS3k@users.noreply.github.com>
2023-04-03 02:54:58 +0000

    new file:   hwcatchup3 	modified:   hwcatchup3.cpp

---
+++ hwcatchup3.cpp
@@ -0,0 +1,28 @@
+/*
+Author: Edmund Gunn Jr
+Device: G-Phone Google Pixel
+Date: 4/2/2023
+Module: 4.1
+Purpose: 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    x=5;
+    cout<<x+2;
+    y=x+2;
+    cout<<x-2;
+    y=x*2;
+    cout<<x/2;
+    x=6;
+    cout<<x=7;
+    y = ( x = 8);
+    y=x=9;
+
+
+
+    return 0;
+}
